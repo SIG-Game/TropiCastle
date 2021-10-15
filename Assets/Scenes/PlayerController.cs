b90fb29356790065ba9411f94ffb2567db91a270
adenprince <45126168+adenprince@users.noreply.github.com>
2021-10-15 18:05:46 -0500

    Added player with horizontal and vertical movement

---
+++ Assets/Scenes/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector2 inputVector = (new Vector2(horizontalInput, verticalInput));
+        inputVector.Normalize();
+
+        rb2d.velocity = movementSpeed * inputVector;
+    }
+}
