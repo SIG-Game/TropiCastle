4d6af0baaa06cc2a955d2ae3755a07fb8370fe71
waleed-khan <wkhan22@illinois.edu>
2021-10-28 23:58:48 -0500

    Started working on melee combat

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,53 @@ public class PlayerController : MonoBehaviour
     public float sprintSpeedMultiplier;
 
     Rigidbody2D rb2d;
-    
-    enum directions {UP, DOWN, LEFT, RIGHT};
+    CapsuleCollider2D hitbox;
+    Direction lastDirection;
+
+    enum Direction { UP, DOWN, LEFT, RIGHT };
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        hitbox = GetComponent<CapsuleCollider2D>();
+        
     }
 
     void Update()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
+
+        bool moving = (horizontalInput != 0 || verticalInput != 0) ? true : false;
+
+        if (moving) {
+            if (horizontalInput < 0)
+            {
+                lastDirection = Direction.LEFT;
+                hitbox.transform.Translate((float) -0.4, 0, 0);
+                Debug.Log("Left");
+            }
+            else if (horizontalInput > 0)
+            {
+                lastDirection = Direction.RIGHT;
+                hitbox.transform.Translate((float) 0.4, 0, 0);
+                Debug.Log("Right");
+            }
+            else if (verticalInput < 0)
+            {
+                lastDirection = Direction.DOWN;
+                hitbox.transform.Translate(0,(float)-0.4, 0);
+                Debug.Log("Down");
+            } 
+            else if (verticalInput > 0) 
+            {
+                lastDirection = Direction.UP;
+                hitbox.transform.Translate(0, (float) 0.4, 0);
+                Debug.Log("Up");
+            }
+        }
+
+        
 
         Vector2 inputVector = (new Vector2(horizontalInput, verticalInput));
         inputVector.Normalize();
@@ -28,7 +63,12 @@ public class PlayerController : MonoBehaviour
         {
             velocity *= sprintSpeedMultiplier;
         }
-
+        Attack();
         rb2d.velocity = velocity;
+    }
+
+
+    void Attack() {
+
     }
 }
