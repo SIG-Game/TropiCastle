f1bde405f17e8c2f27d144d986a9bdc88eff1169
bhashi2 <bhashi2@uic.edu>
2021-12-16 18:58:15 -0600

    added knockback to crabs, working on enemy ai

--- Assets/Scripts/crabScript.cs
+++ Assets/Scripts/crabScript.cs
@@ -7,12 +7,17 @@ public class crabScript : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
 
+    //private Transform target;
+    public float speed;
+
+
     Rigidbody2D rb2d;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        //target = FindObjectOfType<PlayerController>().transform;
     }
 
     // Update is called once per frame
@@ -50,4 +55,25 @@ public class crabScript : MonoBehaviour
             currentHealth = 100;
         }
     }
+
+    //gets knockback when in contact with player
+    //can update to when getting hit by weapon by changing the tag
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Vector2 difference = transform.position - other.transform.position;
+            transform.position = new Vector2(transform.position.x + difference.x, transform.position.y + difference.y);
+        }
+    }
+
+    //testing basic enemy ai
+    //doesn't work yet
+
+    /*
+    public void FollowPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+    */
 }
