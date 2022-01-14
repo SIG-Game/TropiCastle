f46c551d064221be7a0157b9452ea99caae6fb73
adenprince <45126168+adenprince@users.noreply.github.com>
2022-01-13 20:53:47 -0600

    Made interactable character face player when interacted with

--- Assets/Scripts/TestInteractable.cs
+++ Assets/Scripts/TestInteractable.cs
@@ -1,12 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TestInteractable : Interactable
 {
+    public PlayerController player;
+    public Sprite front, back, left, right;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     public override void Interact()
     {
         Debug.Log("Interacted");
+        
+        switch (player.getLastDir())
+        {
+            case PlayerController.Direction.UP:
+                spriteRenderer.sprite = front;
+                break;
+            case PlayerController.Direction.DOWN:
+                spriteRenderer.sprite = back;
+                break;
+            case PlayerController.Direction.LEFT:
+                spriteRenderer.sprite = right;
+                break;
+            case PlayerController.Direction.RIGHT:
+                spriteRenderer.sprite = left;
+                break;
+        }
+
         DialogueBox.Instance.PlayDialogue("Hello.");
     }
 }
