ed3fc1b147534cf62e8a05309672698d5aa38820
adenprince <45126168+adenprince@users.noreply.github.com>
2022-02-16 19:03:00 -0600

    Removed ability to place items on colliders

---
+++ Assets/Scripts/ItemPlacementTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ItemPlacementTrigger : MonoBehaviour
+{
+    public bool canPlace = false;
+
+    void Update()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canPlace = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canPlace = true;
+    }
+}
