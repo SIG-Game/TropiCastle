1b146321574c2470d7f8db9b7a50010b3c536948
Adrian Knight <aknig2@uic.edu>
2022-02-23 19:10:40 -0600

    Basic Level Transition
    Added a crossfade to transition between titleMenu and SampleScene

---
+++ Assets/LevelLoaderScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoaderScript : MonoBehaviour
+{
+   public Animator transition;
+   public float transitionTime = 1f;
+
+    // Update is called once per frame
+    public void makeTransition()
+    {
+       StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
