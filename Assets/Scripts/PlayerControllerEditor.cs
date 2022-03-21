e30f5ae916900e773fdf73f5a48a5ebeeed1bc35
adenprince <45126168+adenprince@users.noreply.github.com>
2022-03-21 15:35:43 -0500

    Added editor UI for adding items to inventory

---
+++ Assets/Scripts/PlayerControllerEditor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PlayerController))]
+public class PlayerControllerEditor : Editor
+{
+    int selectedIndex = 0;
+    String[] options;
+    Inventory inventory;
+
+    private void OnEnable()
+    {
+        options = Enum.GetNames(typeof(Item.ItemType));
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        EditorGUILayout.LabelField("Inventory Debug Controls", style: EditorStyles.boldLabel);
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.PrefixLabel("Item Type");
+        selectedIndex = EditorGUILayout.Popup(selectedIndex, options);
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Add Item"))
+        {
+            if (!Application.isPlaying)
+            {
+                Debug.LogWarning("Cannot add item when not in play mode");
+                return;
+            }
+
+            inventory = ((PlayerController)target).GetInventory();
+            inventory.AddItem((Item.ItemType)selectedIndex, 1);
+        }
+    }
+}
