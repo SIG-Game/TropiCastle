5ad5df882b6dd20724c9552a96d5091c9cfc25bb
bhashi2 <bhashi2@uic.edu>
2022-04-13 18:22:15 -0500

    damageable script

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    private int health = 100;
+
+    public void takeDamage(int damage)
+    {
+        if (health - damage > 0)
+        {
+            health -= damage;
+        }
+        else
+        {
+            health = 0;
+        }
+    }
+}
