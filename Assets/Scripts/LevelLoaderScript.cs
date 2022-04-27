6ff5a4b27e293ccc162d781381be488dc42e6d02
adenprince <45126168+adenprince@users.noreply.github.com>
2022-04-27 18:13:54 -0500

    Added game over UI

--- Assets/Scripts/LevelLoaderScript.cs
+++ Assets/Scripts/LevelLoaderScript.cs
@@ -9,16 +9,17 @@ public class LevelLoaderScript : MonoBehaviour
    public float transitionTime = 1f;
 
     // Update is called once per frame
-    public void makeTransition()
+    public void makeTransition(string sceneName)
     {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        StartCoroutine(LoadLevel(sceneName));
     }
 
-    IEnumerator LoadLevel(int levelIndex)
+    IEnumerator LoadLevel(string sceneName)
     {
         transition.SetTrigger("Start");
-        yield return new WaitForSeconds(transitionTime);
-        SceneManager.LoadScene(levelIndex);
+        yield return new WaitForSecondsRealtime(transitionTime);
+        Time.timeScale = 1f; // TODO: This statement might not always apply, so it could be moved
+        SceneManager.LoadScene(sceneName);
     }
 
 }
