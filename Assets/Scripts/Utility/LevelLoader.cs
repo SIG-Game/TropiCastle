7c2e411adbf46749c408e2acaaff962d6a320dd6
adenprince <45126168+adenprince@users.noreply.github.com>
2022-12-30 13:39:10 -0600

    Rename LevelLoaderScript.cs to LevelLoader.cs and clean up that script
    Also use an animation event in that script instead of a coroutine for loading a scene after the transition animation completes.

---
+++ Assets/Scripts/Utility/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private Animator animator;
+    private string destinationSceneName;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void TransitionToScene(string sceneName)
+    {
+        destinationSceneName = sceneName;
+        animator.SetTrigger("Start");
+    }
+
+    public void OnTransitionCompletedAnimationEvent()
+    {
+        Time.timeScale = 1f; // TODO: This statement might not always apply, so it could be moved
+        SceneManager.LoadScene(destinationSceneName);
+    }
+}
