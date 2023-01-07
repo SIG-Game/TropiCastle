133503e853b0f251d24de18f0efc7993b133c59d
adenprince <45126168+adenprince@users.noreply.github.com>
2023-01-07 15:27:16 -0600

    Remove unused using directives

--- Assets/Scripts/Fishing/ScriptableFish.cs
+++ Assets/Scripts/Fishing/ScriptableFish.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(menuName = "Fish", fileName = "New Fish")]
 public class ScriptableFish : ScriptableObject
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
