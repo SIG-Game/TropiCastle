9ae5c6cf2bbb898860f55bf3e780e99a188196c0
adenprince <45126168+adenprince@users.noreply.github.com>
2023-01-09 21:46:08 -0600

    Rename HookCatch.cs to HookController.cs and clean up that script

---
+++ Assets/Scripts/Fishing/HookController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HookController : MonoBehaviour
+{
+    [SerializeField] private FishingMinigame fishingMinigame;
+
+    private void OnTriggerEnter2D(Collider2D fish)
+    {
+        fishingMinigame.canCatch = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D fish)
+    {
+        fishingMinigame.canCatch = false;
+    }
+}
