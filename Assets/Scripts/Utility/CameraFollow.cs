d27030da624a6451c19a767b9bb64ddac5febc66
adenprince <45126168+adenprince@users.noreply.github.com>
2023-01-10 18:20:40 -0600

    Add CameraFollow.cs and stop using Cinemachine package for camera following
    Also remove Cinemachine package dependency.
    Using Cinemachine camera following and the Pixel Perfect Camera component was causing camera jitter.

---
+++ Assets/Scripts/Utility/CameraFollow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
