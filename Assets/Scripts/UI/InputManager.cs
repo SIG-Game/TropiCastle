85ba5f9a35b523cf1223061064f830ab2cb23630
adenprince <45126168+adenprince@users.noreply.github.com>
2023-01-22 18:16:46 -0600

    Add InputManager.cs with GetLeftClickDownIfUnusedThisFrame method
    Also add Input Manager prefab.

---
+++ Assets/Scripts/UI/InputManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance;
+
+    private bool leftClickInputUsedThisFrame;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Has to run before any scripts that use GetLeftClickDownIfUnusedThisFrame method
+    private void Update()
+    {
+        leftClickInputUsedThisFrame = false;
+    }
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+
+    // Returns left-click down bool when that input has not been used this frame
+    // Otherwise, returns false
+    public bool GetLeftClickDownIfUnusedThisFrame()
+    {
+        if (!leftClickInputUsedThisFrame)
+        {
+            leftClickInputUsedThisFrame = true;
+            return Input.GetMouseButtonDown(0);
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
