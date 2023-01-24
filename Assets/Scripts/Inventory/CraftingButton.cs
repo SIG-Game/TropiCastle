3a19fc4244ee3e7772a750cd2272df00481c8947
adenprince <45126168+adenprince@users.noreply.github.com>
2023-01-23 21:51:50 -0600

    Add automatic generation of crafting buttons from crafting recipe Scriptable Objects
    Also:
    - Rename "Craft Button.prefab" to "Crafting Button.prefab".
    - Move crafting recipe Scriptable Objects from "Assets\Crafting Recipes" folder to "Assets\Resources\Crafting Recipes" folder.

--- Assets/Scripts/Inventory/CraftingButton.cs
+++ Assets/Scripts/Inventory/CraftingButton.cs
@@ -1,12 +1,22 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class CraftingButton : MonoBehaviour
 {
     [SerializeField] private Crafting crafting;
     [SerializeField] private CraftingRecipeScriptableObject craftingRecipe;
+    [SerializeField] private TextMeshProUGUI craftingButtonText;
 
     public void CraftingButton_OnClick()
     {
         crafting.CraftItem(craftingRecipe);
+    }
+
+    public void SetUpCraftingButton(Crafting crafting, CraftingRecipeScriptableObject craftingRecipe)
+    {
+        this.crafting = crafting;
+        this.craftingRecipe = craftingRecipe;
+
+        craftingButtonText.text = $"Craft {craftingRecipe.name}";
     }
 }
---
+++ Assets/Scripts/Inventory/CraftingButtonGenerator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CraftingButtonGenerator : MonoBehaviour
+{
+    [SerializeField] private Crafting crafting;
+    [SerializeField] private GameObject craftingButtonPrefab;
+    [SerializeField] private Transform craftingButtonsParentTransform;
+    [SerializeField] private int yDistanceBetweenButtons;
+
+    private void Awake()
+    {
+        CraftingRecipeScriptableObject[] craftingRecipes = Resources.LoadAll<CraftingRecipeScriptableObject>("Crafting Recipes");
+
+        Vector3 craftingButtonPosition = craftingButtonsParentTransform.position;
+
+        for (int i = 0; i < craftingRecipes.Length; ++i)
+        {
+            GameObject craftingButton = Instantiate(craftingButtonPrefab, craftingButtonPosition, Quaternion.identity);
+            craftingButton.name = $"Craft {craftingRecipes[i].name} Button";
+
+            RectTransform craftingButtonRectTransform = craftingButton.GetComponent<RectTransform>();
+            craftingButtonRectTransform.anchorMin = new Vector2(0.5f, 0.5f);
+            craftingButtonRectTransform.anchorMax = new Vector2(0.5f, 0.5f);
+
+            craftingButton.transform.SetParent(craftingButtonsParentTransform);
+
+            craftingButton.GetComponent<CraftingButton>().SetUpCraftingButton(crafting, craftingRecipes[i]);
+
+            craftingButtonPosition.y += yDistanceBetweenButtons;
+        }
+    }
+}
