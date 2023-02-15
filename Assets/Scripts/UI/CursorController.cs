d69f035668e2dde31fc0878127e0e9394382b199
adenprince <45126168+adenprince@users.noreply.github.com>
2023-02-14 19:37:50 -0600

    Fix in-game mouse cursor being visible while game is paused
    Also add OnGamePaused and OnGameUnpaused events to PauseController.cs.

--- Assets/Scripts/UI/CursorController.cs
+++ Assets/Scripts/UI/CursorController.cs
@@ -12,17 +12,26 @@ public class CursorController : MonoBehaviour
     {
         cursorSpriteRenderer = GetComponent<SpriteRenderer>();
         cursorBackgroundSpriteRenderer = cursorBackground.GetComponent<SpriteRenderer>();
+
+        PauseController.OnGamePaused += PauseController_OnGamePaused;
+        PauseController.OnGameUnpaused += PauseController_OnGameUnpaused;
     }
 
     private void Update()
     {
         if (PauseController.Instance.GamePaused)
         {
-            UseDefaultCursor();
+            return;
         }
 
         Vector2 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = mouseWorldPoint;
+    }
+
+    private void OnDestroy()
+    {
+        PauseController.OnGamePaused -= PauseController_OnGamePaused;
+        PauseController.OnGameUnpaused -= PauseController_OnGameUnpaused;
     }
 
     public void SetCursorSprite(Sprite sprite)
@@ -44,5 +53,21 @@ public class CursorController : MonoBehaviour
     public void SetCursorBackgroundLocalScale(Vector3 localScale)
     {
         cursorBackground.transform.localScale = localScale;
+    }
+
+    private void HideCursor()
+    {
+        cursorSpriteRenderer.sprite = null;
+        cursorBackgroundSpriteRenderer.color = Color.clear;
+    }
+
+    private void PauseController_OnGamePaused()
+    {
+        HideCursor();
+    }
+
+    private void PauseController_OnGameUnpaused()
+    {
+        UseDefaultCursor();
     }
 }
--- Assets/Scripts/Utility/PauseController.cs
+++ Assets/Scripts/Utility/PauseController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PauseController : MonoBehaviour
 {
@@ -11,10 +12,22 @@ public class PauseController : MonoBehaviour
         {
             gamePaused = value;
             Time.timeScale = gamePaused ? 0f : 1f;
+
+            if (gamePaused)
+            {
+                OnGamePaused();
+            }
+            else
+            {
+                OnGameUnpaused();
+            }
         }
     }
 
     public static PauseController Instance;
+
+    public static event Action OnGamePaused;
+    public static event Action OnGameUnpaused;
 
     private void Awake()
     {
