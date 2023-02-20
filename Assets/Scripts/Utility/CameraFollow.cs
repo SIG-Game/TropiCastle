15bda8ebbb4c22f6abaf9b763147262a29c4901b
adenprince <45126168+adenprince@users.noreply.github.com>
2023-02-19 22:48:31 -0600

    Restrict camera view to inside ocean tilemap bounds

--- Assets/Scripts/Utility/CameraFollow.cs
+++ Assets/Scripts/Utility/CameraFollow.cs
@@ -1,11 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private Tilemap tilemap;
+
+    private new Camera camera;
+
+    private float tilemapMinX;
+    private float tilemapMaxX;
+    private float tilemapMinY;
+    private float tilemapMaxY;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+
+        tilemapMinX = tilemap.localBounds.min.x;
+        tilemapMaxX = tilemap.localBounds.max.x;
+        tilemapMinY = tilemap.localBounds.min.y;
+        tilemapMaxY = tilemap.localBounds.max.y;
+    }
 
     private void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        float halfCameraHeight = camera.orthographicSize;
+        float halfCameraWidth = camera.orthographicSize * camera.aspect;
+
+        float minCameraX = tilemapMinX + halfCameraWidth;
+        float maxCameraX = tilemapMaxX - halfCameraWidth;
+
+        float minCameraY = tilemapMinY + halfCameraHeight;
+        float maxCameraY = tilemapMaxY - halfCameraHeight;
+
+        float newXPosition = Mathf.Clamp(target.position.x, minCameraX, maxCameraX);
+        float newYPosition = Mathf.Clamp(target.position.y, minCameraY, maxCameraY);
+
+        Vector3 newPosition = new Vector3(newXPosition, newYPosition, transform.position.z);
+        transform.position = newPosition;
     }
 }
