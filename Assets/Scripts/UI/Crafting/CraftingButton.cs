b01ba1746ee1ee14590fdcd4ab864489010ca21e
adenprince <45126168+adenprince@users.noreply.github.com>
2023-03-11 23:28:10 -0600

    Use text color in crafting button tooltip to indicate which ingredients the player has in their inventory
    A green ingredient item name in the tooltip means that the player has that item in their inventory, and a red name means that the player does not.

--- Assets/Scripts/UI/Crafting/CraftingButton.cs
+++ Assets/Scripts/UI/Crafting/CraftingButton.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -8,6 +9,7 @@ public class CraftingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     [SerializeField] private Crafting crafting;
     [SerializeField] private CraftingRecipeScriptableObject craftingRecipe;
     [SerializeField] private Image craftingButtonImage;
+    [SerializeField] private Inventory playerInventory;
 
     public void CraftingButton_OnClick()
     {
@@ -36,9 +38,31 @@ public class CraftingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     {
         StringBuilder ingredientsStringBuilder = new StringBuilder("Ingredients:\n");
 
+        List<ItemWithAmount> playerInventoryItemList = playerInventory.GetItemList();
+
+        // Get indexes of inventory items used when checking if the player's inventory has an ingredient
+        // so that an inventory item is not reused when multiple ingredients have the same item name
+        List<int> itemIndexesUsed = new List<int>();
+
         foreach (ItemWithAmount ingredient in craftingRecipe.ingredients)
         {
-            ingredientsStringBuilder.AppendLine("- " + ingredient.itemData.name);
+            bool playerHasIngredient = false;
+
+            for (int i = 0; i < playerInventoryItemList.Count; ++i)
+            {
+                ItemWithAmount currentItem = playerInventoryItemList[i];
+
+                if (currentItem.itemData.name == ingredient.itemData.name && !itemIndexesUsed.Contains(i))
+                {
+                    playerHasIngredient = true;
+                    itemIndexesUsed.Add(i);
+                    break;
+                }
+            }
+
+            ingredientsStringBuilder.Append(playerHasIngredient ? "<color=#00FF00>" : "<color=#FF0000>");
+            ingredientsStringBuilder.Append("- " + ingredient.itemData.name + "</color>");
+            ingredientsStringBuilder.AppendLine();
         }
 
         return ingredientsStringBuilder.ToString();
