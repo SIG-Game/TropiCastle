ba9432d51e15fd3ea09d355e469a123740ca316d
adenprince <45126168+adenprince@users.noreply.github.com>
2023-03-31 20:43:57 -0500

    Make player damage text wait for a set time before fading out
    Also increase player damage text fade out speed.

--- Assets/Scripts/UI/DamageTextUIController.cs
+++ Assets/Scripts/UI/DamageTextUIController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -5,24 +6,44 @@ public class DamageTextUIController : MonoBehaviour
 {
     [SerializeField] private TMP_Text damageText;
     [SerializeField] private float yVelocity;
+    [SerializeField] private float waitSecondsBeforeFadeOut;
     [SerializeField] private float fadeOutSpeed;
+
+    private WaitForSeconds beforeFadeOutWaitForSecondsObject;
+
+    private void Awake()
+    {
+        beforeFadeOutWaitForSecondsObject = new WaitForSeconds(waitSecondsBeforeFadeOut);
+
+        StartCoroutine(WaitThenFadeOut());
+    }
 
     private void Update()
     {
         float newYPosition = transform.position.y + yVelocity * Time.deltaTime;
 
         transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
+    }
 
-        float newAlpha = damageText.color.a - fadeOutSpeed * Time.deltaTime;
+    private IEnumerator WaitThenFadeOut()
+    {
+        yield return beforeFadeOutWaitForSecondsObject;
 
-        if (newAlpha <= 0f)
+        while (damageText.color.a > 0f)
         {
-            newAlpha = 0f;
+            yield return null;
 
-            Destroy(gameObject);
+            float newAlpha = damageText.color.a - fadeOutSpeed * Time.deltaTime;
+
+            if (newAlpha <= 0f)
+            {
+                newAlpha = 0f;
+
+                Destroy(gameObject);
+            }
+
+            damageText.color = new Color(damageText.color.r, damageText.color.g, damageText.color.b, newAlpha);
         }
-
-        damageText.color = new Color(damageText.color.r, damageText.color.g, damageText.color.b, newAlpha);
     }
 
     public void SetUpDamageText(int damage)
