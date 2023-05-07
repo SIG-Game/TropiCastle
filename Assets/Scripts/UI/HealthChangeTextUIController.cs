7c57527a57e99622267120d48bb630ea06460f83
adenprince <45126168+adenprince@users.noreply.github.com>
2023-05-07 17:49:12 -0500

    Add background to health change text

--- Assets/Scripts/UI/HealthChangeTextUIController.cs
+++ Assets/Scripts/UI/HealthChangeTextUIController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthChangeTextUIController : MonoBehaviour
 {
     [SerializeField] private TMP_Text healthChangeText;
+    [SerializeField] private RectTransform backgroundRectTransform;
+    [SerializeField] private SpriteRenderer backgroundSpriteRenderer;
     [SerializeField] private Color damageTextColor;
     [SerializeField] private Color healTextColor;
     [SerializeField] private float yVelocity;
@@ -12,12 +15,27 @@ public class HealthChangeTextUIController : MonoBehaviour
     [SerializeField] private float fadeOutSpeed;
 
     private WaitForSeconds beforeFadeOutWaitForSecondsObject;
+    private float initialBackgroundAlpha;
 
     private void Awake()
     {
         beforeFadeOutWaitForSecondsObject = new WaitForSeconds(waitSecondsBeforeFadeOut);
 
+        initialBackgroundAlpha = backgroundSpriteRenderer.color.a;
+
         StartCoroutine(WaitThenFadeOut());
+    }
+
+    private void Start()
+    {
+        // This method call doesn't work in the Awake method
+        LayoutRebuilder.ForceRebuildLayoutImmediate(
+            healthChangeText.GetComponent<RectTransform>());
+
+        backgroundRectTransform.localScale = new Vector3(
+            healthChangeText.preferredWidth,
+            backgroundRectTransform.localScale.y,
+            backgroundRectTransform.localScale.z);
     }
 
     private void Update()
@@ -31,21 +49,29 @@ public class HealthChangeTextUIController : MonoBehaviour
     {
         yield return beforeFadeOutWaitForSecondsObject;
 
+        float backgroundFadeOutSpeed = fadeOutSpeed * initialBackgroundAlpha;
+
         while (healthChangeText.color.a > 0f)
         {
             yield return null;
 
             float newAlpha = healthChangeText.color.a - fadeOutSpeed * Time.deltaTime;
+            float newBackgroundAlpha = backgroundSpriteRenderer.color.a -
+                backgroundFadeOutSpeed * Time.deltaTime;
 
             if (newAlpha <= 0f)
             {
                 newAlpha = 0f;
+                newBackgroundAlpha = 0f;
 
                 Destroy(gameObject);
             }
 
             healthChangeText.color = new Color(healthChangeText.color.r, healthChangeText.color.g,
                 healthChangeText.color.b, newAlpha);
+            backgroundSpriteRenderer.color = new Color(backgroundSpriteRenderer.color.r,
+                backgroundSpriteRenderer.color.g, backgroundSpriteRenderer.color.b,
+                newBackgroundAlpha);
         }
     }
 
