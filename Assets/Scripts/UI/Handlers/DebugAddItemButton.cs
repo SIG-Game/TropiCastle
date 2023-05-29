882c4215c2c2ab521460eb5443e146d6b876efb1
adenprince <45126168+adenprince@users.noreply.github.com>
2023-05-28 21:08:33 -0500

    Add debug add item amount text input field

--- Assets/Scripts/UI/Handlers/DebugAddItemButton.cs
+++ Assets/Scripts/UI/Handlers/DebugAddItemButton.cs
@@ -1,12 +1,24 @@
+using TMPro;
 using UnityEngine;
 
 public class DebugAddItemButton : MonoBehaviour
 {
     [SerializeField] private DebugAddItemDropdownController addItemDropdownController;
+    [SerializeField] private TMP_InputField amountInputField;
     [SerializeField] private Inventory playerInventory;
 
     public void DebugAddItemButton_OnClick()
     {
-        playerInventory.AddItem(addItemDropdownController.GetSelectedItemScriptableObject(), 1);
+        ItemScriptableObject selectedItemScriptableObject =
+            addItemDropdownController.GetSelectedItemScriptableObject();
+
+        if (int.TryParse(amountInputField.text, out int amount))
+        {
+            playerInventory.AddItem(selectedItemScriptableObject, amount);
+        }
+        else
+        {
+            playerInventory.AddItem(selectedItemScriptableObject, 1);
+        }
     }
 }
