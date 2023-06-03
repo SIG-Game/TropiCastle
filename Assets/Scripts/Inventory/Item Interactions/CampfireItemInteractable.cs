8b1053da4d9e6850d66318ba49b1403c6eb44e2e
adenprince <45126168+adenprince@users.noreply.github.com>
2023-06-03 16:55:19 -0500

    Update CampfireItemInteractable.cs
    Also fix interacting with a campfire removing all raw crab meat items from an item stack in the player's inventory. Now, only one raw crab meat item is removed from an item stack in the player's inventory when interacting with a campfire.

--- Assets/Scripts/Inventory/Item Interactions/CampfireItemInteractable.cs
+++ Assets/Scripts/Inventory/Item Interactions/CampfireItemInteractable.cs
@@ -1,25 +1,47 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CampfireItemInteractable : Interactable
 {
     private static readonly Lazy<ItemScriptableObject> lazyCookedCrabMeat =
-        new Lazy<ItemScriptableObject>(LoadCookedCrabMeatItemScriptableObject);
+        new Lazy<ItemScriptableObject>(() => LoadItemScriptableObject("CookedCrabMeat"));
+
+    private static List<CampfireRecipe> campfireRecipes = new List<CampfireRecipe>
+    {
+        new CampfireRecipe("Raw Crab Meat", lazyCookedCrabMeat)
+    };
 
     public override void Interact(PlayerController playerController)
     {
         Inventory playerInventory = playerController.GetInventory();
 
-        int rawCrabMeatIndex = playerInventory.GetItemList()
-            .FindIndex(x => x.itemData.name == "Raw Crab Meat");
-
-        if (rawCrabMeatIndex != -1)
+        foreach (CampfireRecipe campfireRecipe in campfireRecipes)
         {
-            playerInventory.RemoveItemAtIndex(rawCrabMeatIndex);
-            playerInventory.AddItem(lazyCookedCrabMeat.Value, 1);
+            int inputItemIndex = playerInventory.GetItemList()
+                .FindIndex(x => x.itemData.name == campfireRecipe.InputItemName);
+
+            if (inputItemIndex != -1)
+            {
+                playerInventory.DecrementItemStackAtIndex(inputItemIndex);
+
+                playerInventory.AddItem(campfireRecipe.LazyResultItem.Value, 1);
+            }
         }
     }
 
-    private static ItemScriptableObject LoadCookedCrabMeatItemScriptableObject() =>
-        Resources.Load<ItemScriptableObject>("Items/CookedCrabMeat");
+    private static ItemScriptableObject LoadItemScriptableObject(string name) =>
+        Resources.Load<ItemScriptableObject>($"Items/{name}");
+
+    private class CampfireRecipe
+    {
+        public string InputItemName;
+        public Lazy<ItemScriptableObject> LazyResultItem;
+
+        public CampfireRecipe(string inputItemName, Lazy<ItemScriptableObject> lazyResultItem)
+        {
+            InputItemName = inputItemName;
+            LazyResultItem = lazyResultItem;
+        }
+    }
 }
