a4ad71d8cb210bdfb3a2f9605fc340b93f5c99d8
adenprince <45126168+adenprince@users.noreply.github.com>
2023-07-01 19:59:34 -0500

    Prevent thrown items from spawning inside colliders
    Also add Player layer.

--- Assets/Scripts/Player/Item Usages/ThrowableItemUsage.cs
+++ Assets/Scripts/Player/Item Usages/ThrowableItemUsage.cs
@@ -6,14 +6,38 @@ public class ThrowableItemUsage : MonoBehaviour, IItemUsage
     [SerializeField] private Inventory playerInventory;
     [SerializeField] private Collider2D playerCollider2D;
     [SerializeField] private CursorController cursorController;
+    [SerializeField] private float canThrowBoxCastDistance;
+
+    private LayerMask notPlayerLayerMask;
+
+    private void Awake()
+    {
+        notPlayerLayerMask = ~LayerMask.GetMask("Player");
+    }
 
     public void UseItem(ItemWithAmount item, int itemIndex)
     {
-        ItemWithAmount itemToThrow = new ItemWithAmount(item);
-        itemToThrow.amount = 1;
-
         Vector3 thrownItemStartPosition =
             playerController.transform.position + new Vector3(0f, 0.3f, 0f);
+
+        Vector3 throwDirection =
+            ((Vector3)cursorController.GetWorldPosition() - thrownItemStartPosition).normalized;
+
+        Vector2 itemColliderSize =
+            ItemWorldPrefabInstanceFactory.GetItemColliderSize(item.itemData);
+
+        Physics2D.queriesHitTriggers = false;
+        bool canThrowItem = Physics2D.BoxCast(thrownItemStartPosition, itemColliderSize, 0f,
+            throwDirection, canThrowBoxCastDistance, notPlayerLayerMask).collider == null;
+        Physics2D.queriesHitTriggers = true;
+
+        if (!canThrowItem)
+        {
+            return;
+        }
+
+        ItemWithAmount itemToThrow = new ItemWithAmount(item);
+        itemToThrow.amount = 1;
 
         ItemWorld thrownItemWorld =
             ItemWorldPrefabInstanceFactory.Instance.SpawnItemWorld(
@@ -21,9 +45,6 @@ public class ThrowableItemUsage : MonoBehaviour, IItemUsage
 
         ThrownItemWorld thrownItemWorldComponent =
             thrownItemWorld.gameObject.AddComponent<ThrownItemWorld>();
-
-        Vector3 throwDirection =
-            ((Vector3)cursorController.GetWorldPosition() - thrownItemStartPosition).normalized;
 
         thrownItemWorldComponent.SetUpThrownItemWorld(throwDirection,
             (ThrowableItemScriptableObject)itemToThrow.itemData, playerCollider2D);
