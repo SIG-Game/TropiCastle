1e78eea9de6deac5c0c2715a6e66e255c9433e9d
adenprince <45126168+adenprince@users.noreply.github.com>
2023-07-07 17:39:53 -0500

    Fix Player Inventory UI Controllers prefab
    Previously, that prefab had component properties that were incorrectly unassigned.

--- Assets/Scripts/UI/Inventory/InventoryUIManager.cs
+++ Assets/Scripts/UI/Inventory/InventoryUIManager.cs
@@ -5,6 +5,7 @@ public class InventoryUIManager : MonoBehaviour
 {
     public event Action OnInventoryUIClosed = delegate { };
 
+    // TODO: Make non-static
     public static bool InventoryUIOpen;
 
     static InventoryUIManager()
