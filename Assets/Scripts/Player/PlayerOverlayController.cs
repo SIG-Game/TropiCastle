f411fe85e56659d03acd1c1428e5bd10775a6856
adenprince <45126168+adenprince@users.noreply.github.com>
2023-07-08 17:45:48 -0500

    Clean up PlayerOverlayController.cs

--- Assets/Scripts/Player/PlayerOverlayController.cs
+++ Assets/Scripts/Player/PlayerOverlayController.cs
@@ -9,7 +9,7 @@ public class PlayerOverlayController : MonoBehaviour
     [SerializeField] private float alphaChangeSpeed;
 
     private SpriteRenderer spriteRenderer;
-    private Coroutine startFadingOutAfterWaitCoroutineObject;
+    private Coroutine startFadingOutAfterWaitCoroutine;
     private WaitForSeconds maxAlphaWaitForSeconds;
     private float targetAlpha;
 
@@ -21,27 +21,25 @@ public class PlayerOverlayController : MonoBehaviour
 
         targetAlpha = 0f;
 
-        playerHealthController.OnHealthChangedByAmount += HealthController_OnHealthChangedByAmount;
+        playerHealthController.OnHealthChangedByAmount +=
+            HealthController_OnHealthChangedByAmount;
     }
 
     private void Update()
     {
         if (spriteRenderer.color.a != targetAlpha)
         {
-            float newAlpha = Mathf.MoveTowards(spriteRenderer.color.a, targetAlpha,
-                alphaChangeSpeed * Time.deltaTime);
+            float newAlpha = Mathf.MoveTowards(spriteRenderer.color.a,
+                targetAlpha, alphaChangeSpeed * Time.deltaTime);
 
-            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g,
-                spriteRenderer.color.b, newAlpha);
+            SetAlpha(newAlpha);
         }
     }
 
     private void OnDestroy()
     {
-        if (playerHealthController != null)
-        {
-            playerHealthController.OnHealthChangedByAmount -= HealthController_OnHealthChangedByAmount;
-        }
+        playerHealthController.OnHealthChangedByAmount -=
+            HealthController_OnHealthChangedByAmount;
     }
 
     private IEnumerator StartFadingOutAfterWaitCoroutine()
@@ -51,20 +49,26 @@ public class PlayerOverlayController : MonoBehaviour
         targetAlpha = 0f;
     }
 
+    private void SetAlpha(float alpha)
+    {
+        spriteRenderer.color = new Color(spriteRenderer.color.r,
+            spriteRenderer.color.g, spriteRenderer.color.b, alpha);
+    }
+
     private void HealthController_OnHealthChangedByAmount(int healthDelta)
     {
         if (healthDelta < 0)
         {
-            if (startFadingOutAfterWaitCoroutineObject != null)
+            if (startFadingOutAfterWaitCoroutine != null)
             {
-                StopCoroutine(startFadingOutAfterWaitCoroutineObject);
+                StopCoroutine(startFadingOutAfterWaitCoroutine);
             }
 
-            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g,
-                spriteRenderer.color.b, maxAlpha);
-            targetAlpha = spriteRenderer.color.a;
+            SetAlpha(maxAlpha);
 
-            startFadingOutAfterWaitCoroutineObject =
+            targetAlpha = maxAlpha;
+
+            startFadingOutAfterWaitCoroutine =
                 StartCoroutine(StartFadingOutAfterWaitCoroutine());
         }
     }
