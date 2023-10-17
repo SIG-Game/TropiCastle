96df27e4cde679ca769ea7472ee5b04de100bf11
adenprince <45126168+adenprince@users.noreply.github.com>
2023-10-16 21:11:39 -0500

    Add recipe list to campfire UI

---
+++ Assets/Scripts/UI/CampfireRecipeListUIController.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class CampfireRecipeListUIController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI recipeListText;
+
+    private void Awake()
+    {
+        var campfireRecipesLoadHandle =
+            Addressables.LoadAssetsAsync<CampfireRecipeScriptableObject>(
+                "campfire recipe", null);
+
+        IList<CampfireRecipeScriptableObject> campfireRecipes =
+            campfireRecipesLoadHandle.WaitForCompletion();
+
+        Addressables.Release(campfireRecipesLoadHandle);
+
+        var recipeListBuilder = new StringBuilder();
+
+        foreach (CampfireRecipeScriptableObject recipe in campfireRecipes)
+        {
+            foreach (ItemStack inputItem in recipe.PossibleInputItems)
+            {
+                recipeListBuilder.AppendLine(
+                    $"{inputItem.amount} {inputItem.itemDefinition.name} -> " +
+                    $"{recipe.ResultItem.amount} {recipe.ResultItem.itemDefinition.name}");
+            }
+        }
+
+        recipeListText.text = recipeListBuilder.ToString();
+    }
+}
