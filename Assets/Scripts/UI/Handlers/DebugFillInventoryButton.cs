7022da53ec871c438b647aa2a0bf2adc9d92f4fb
adenprince <45126168+adenprince@users.noreply.github.com>
2023-12-10 21:20:26 -0600

    Make fill inventory button hide held item UI

--- Assets/Scripts/UI/Handlers/DebugFillInventoryButton.cs
+++ Assets/Scripts/UI/Handlers/DebugFillInventoryButton.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class DebugFillInventoryButton : MonoBehaviour
 {
+    [SerializeField] private InventoryUIHeldItemController inventoryUIHeldItemController;
     [SerializeField] private Inventory inventory;
 
     public void DebugFillInventoryButton_OnClick()
     {
+        if (inventoryUIHeldItemController.HoldingItem())
+        {
+            inventoryUIHeldItemController.HideHeldItemUI();
+        }
+
         inventory.FillInventory();
     }
 }
