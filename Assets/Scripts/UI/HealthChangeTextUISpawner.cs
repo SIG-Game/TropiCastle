8cf8bc825847cb3f0a88429d58799112d217a39b
adenprince <45126168+adenprince@users.noreply.github.com>
2023-12-23 21:45:24 -0600

    Refactor HealthChangeTextUISpawner.cs

--- Assets/Scripts/UI/HealthChangeTextUISpawner.cs
+++ Assets/Scripts/UI/HealthChangeTextUISpawner.cs
@@ -2,31 +2,30 @@ using UnityEngine;
 
 public class HealthChangeTextUISpawner : MonoBehaviour
 {
-    [SerializeField] private HealthController targetHealthController;
+    [SerializeField] private HealthController healthController;
     [SerializeField] private GameObject healthChangeTextPrefab;
 
     private void Awake()
     {
-        targetHealthController.OnHealthChangedByAmount += HealthController_OnHealthChangedByAmount;
+        healthController.OnHealthChangedByAmount +=
+            HealthController_OnHealthChangedByAmount;
     }
 
     private void OnDestroy()
     {
-        if (targetHealthController != null)
-        {
-            targetHealthController.OnHealthChangedByAmount -= HealthController_OnHealthChangedByAmount;
-        }
+        healthController.OnHealthChangedByAmount -=
+            HealthController_OnHealthChangedByAmount;
     }
 
     private void HealthController_OnHealthChangedByAmount(int healthDelta)
     {
         if (healthDelta != 0 && !PauseController.Instance.GamePaused)
         {
-            GameObject healthChangeTextGameObject = Instantiate(healthChangeTextPrefab, transform);
-            HealthChangeTextUIController healthChangeTextUIController =
-                healthChangeTextGameObject.GetComponent<HealthChangeTextUIController>();
-
-            healthChangeTextUIController.SetUpHealthChangeText(healthDelta);
+            GameObject healthChangeText =
+                Instantiate(healthChangeTextPrefab, transform);
+            
+            healthChangeText.GetComponent<HealthChangeTextUIController>()
+                .SetUpHealthChangeText(healthDelta);
         }
     }
 }
