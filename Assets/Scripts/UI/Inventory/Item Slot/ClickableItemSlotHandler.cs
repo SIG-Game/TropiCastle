4f513be41788680a58e09bec7bcc7ef2b29a8343
adenprince <45126168+adenprince@users.noreply.github.com>
2024-01-06 22:41:09 -0600

    Refactor ClickableItemSlotHandler.cs

--- Assets/Scripts/UI/Inventory/Item Slot/ClickableItemSlotHandler.cs
+++ Assets/Scripts/UI/Inventory/Item Slot/ClickableItemSlotHandler.cs
@@ -16,29 +16,20 @@ public class ClickableItemSlotHandler : MonoBehaviour, IElementWithTooltip,
         set => inventory = value;
     }
 
-    public int SlotItemIndex
-    {
-        private get => slotItemIndex;
-        set => slotItemIndex = value;
-    }
-
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (hoveredItemSlotManager != null)
-        {
-            hoveredItemSlotManager.HoveredItemIndex = SlotItemIndex;
-            hoveredItemSlotManager.HoveredInventory = Inventory;
-        }
+        hoveredItemSlotManager.HoveredInventory = Inventory;
+        hoveredItemSlotManager.HoveredItemIndex = slotItemIndex;
 
         if (Input.GetMouseButton(0))
         {
             inventoryUIHeldItemController.HeldLeftClickOverItemAtIndex(
-                Inventory, SlotItemIndex);
+                Inventory, slotItemIndex);
         }
         else if (Input.GetMouseButton(1) && itemPlacementEnabled)
         {
             inventoryUIHeldItemController.HeldRightClickOverItemAtIndex(
-                Inventory, SlotItemIndex);
+                Inventory, slotItemIndex);
         }
     }
 
@@ -47,24 +38,21 @@ public class ClickableItemSlotHandler : MonoBehaviour, IElementWithTooltip,
         if (eventData.button == PointerEventData.InputButton.Left)
         {
             inventoryUIHeldItemController.LeftClickedItemAtIndex(
-                Inventory, SlotItemIndex, itemPlacementEnabled);
+                Inventory, slotItemIndex, itemPlacementEnabled);
         }
         else if (eventData.button == PointerEventData.InputButton.Right &&
             itemPlacementEnabled)
         {
             inventoryUIHeldItemController.RightClickedItemAtIndex(
-                Inventory, SlotItemIndex);
+                Inventory, slotItemIndex);
         }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (hoveredItemSlotManager != null)
-        {
-            hoveredItemSlotManager.HoveredItemIndex = -1;
-        }
+        hoveredItemSlotManager.HoveredItemIndex = -1;
     }
 
     public string GetTooltipText() =>
-        Inventory.GetItemAtIndex(SlotItemIndex).GetTooltipText();
+        Inventory.GetItemAtIndex(slotItemIndex).GetTooltipText();
 }
