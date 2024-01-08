bd4e822805489421efd2dcb01e06d141e2023ca9
adenprince <45126168+adenprince@users.noreply.github.com>
2024-01-07 20:02:35 -0600

    Use dependency injection in ChestUIController.cs

--- Assets/Scripts/UI/Inventory/Item Slot Containers/ChestUIController.cs
+++ Assets/Scripts/UI/Inventory/Item Slot Containers/ChestUIController.cs
@@ -6,8 +6,14 @@ public class ChestUIController : MonoBehaviour
     [SerializeField] private List<GameObject> chestUIGameObjects;
     [SerializeField] private InventoryUIController chestInventoryUIController;
     [SerializeField] private RectTransform playerInventoryUI;
-    [SerializeField] private InventoryUIManager inventoryUIManager;
     [SerializeField] private Vector2 playerInventoryUIPosition;
+
+    [Inject] private InventoryUIManager inventoryUIManager;
+
+    private void Awake()
+    {
+        this.InjectDependencies();
+    }
 
     public void ShowChestUI()
     {
