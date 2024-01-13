78d4b01f0113e7cd4ade2250d4ba5d6684df8fdd
adenprince <45126168+adenprince@users.noreply.github.com>
2024-01-12 20:24:31 -0600

    Display player inventory UI in seller NPC UI

--- Assets/Scripts/UI/SellerNPCUIController.cs
+++ Assets/Scripts/UI/SellerNPCUIController.cs
@@ -1,14 +1,19 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SellerNPCUIController : MonoBehaviour
 {
     [SerializeField] private List<GameObject> sellerNPCUIGameObjects;
     [SerializeField] private TextMeshProUGUI productText;
+    [SerializeField] private Button buyButton;
+    [SerializeField] private RectTransform playerInventoryUI;
     [SerializeField] private Inventory playerInventory;
     [SerializeField] private MoneyController playerMoneyController;
+    [SerializeField] private Vector2 playerInventoryUIPosition;
 
+    [Inject] private InventoryUIHeldItemController inventoryUIHeldItemController;
     [Inject] private InventoryUIManager inventoryUIManager;
 
     private NPCProductScriptableObject product;
@@ -16,6 +21,19 @@ public class SellerNPCUIController : MonoBehaviour
     private void Awake()
     {
         this.InjectDependencies();
+
+        inventoryUIHeldItemController.OnItemHeld +=
+            InventoryUIHeldItemController_OnItemHeld;
+        inventoryUIHeldItemController.OnHidden +=
+            InventoryUIHeldItemController_OnHidden;
+    }
+
+    private void OnDestroy()
+    {
+        inventoryUIHeldItemController.OnItemHeld -=
+            InventoryUIHeldItemController_OnItemHeld;
+        inventoryUIHeldItemController.OnHidden -=
+            InventoryUIHeldItemController_OnHidden;
     }
 
     public void DisplayProduct(NPCProductScriptableObject product)
@@ -23,6 +41,8 @@ public class SellerNPCUIController : MonoBehaviour
         this.product = product;
 
         productText.text = $"{product.Item} For {product.Cost} Money";
+
+        playerInventoryUI.anchoredPosition = playerInventoryUIPosition;
 
         inventoryUIManager.SetCurrentInventoryUIGameObjects(sellerNPCUIGameObjects);
         inventoryUIManager.EnableCurrentInventoryUI();
@@ -38,4 +58,10 @@ public class SellerNPCUIController : MonoBehaviour
             playerMoneyController.Money -= product.Cost;
         }
     }
+
+    private void InventoryUIHeldItemController_OnItemHeld() =>
+        buyButton.interactable = false;
+
+    private void InventoryUIHeldItemController_OnHidden() =>
+        buyButton.interactable = true;
 }
