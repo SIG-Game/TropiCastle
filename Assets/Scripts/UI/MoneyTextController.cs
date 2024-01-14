a78f9e2f0976b2e3181f7118d0146e3e17eeee7b
adenprince <45126168+adenprince@users.noreply.github.com>
2024-01-13 20:48:08 -0600

    Display player money UI in seller NPC UI

--- Assets/Scripts/UI/MoneyTextController.cs
+++ Assets/Scripts/UI/MoneyTextController.cs
@@ -3,14 +3,11 @@ using UnityEngine;
 
 public class MoneyTextController : MonoBehaviour
 {
+    [SerializeField] private TMP_Text moneyText;
     [SerializeField] private MoneyController moneyController;
-    
-    private TMP_Text moneyText;
 
     private void Awake()
     {
-        moneyText = GetComponent<TMP_Text>();
-
         moneyController.OnMoneySet += MoneyController_OnMoneySet;
     }
 
