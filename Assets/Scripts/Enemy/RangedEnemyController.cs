68a14013ab021d30271cd59881699de2e59085c8
adenprince <45126168+adenprince@users.noreply.github.com>
2024-01-14 20:08:00 -0600

    Use dependency injection in a script
    That script is RangedEnemyController.cs.

--- Assets/Scripts/Enemy/RangedEnemyController.cs
+++ Assets/Scripts/Enemy/RangedEnemyController.cs
@@ -2,25 +2,28 @@ using UnityEngine;
 
 public class RangedEnemyController : MonoBehaviour
 {
-    [SerializeField] private Transform player;
     [SerializeField] private GameObject bullet;
     [SerializeField] private float targetRange;
     [SerializeField] private float waitTimeBeforeShootingSeconds;
     [SerializeField] private float timeBetweenShotsSeconds;
 
+    [Inject("PlayerTransform")] private Transform playerTransform;
+
     private float shootTime;
 
     private void Awake()
     {
+        this.InjectDependencies();
+
         shootTime = waitTimeBeforeShootingSeconds;
     }
 
     private void Update()
     {
-        Vector2 directionToPlayer = player.position - transform.position;
+        Vector2 directionToPlayer = playerTransform.position - transform.position;
         transform.up = directionToPlayer;
 
-        if (Vector3.Distance(transform.position, player.position) <= targetRange &&
+        if (Vector3.Distance(transform.position, playerTransform.position) <= targetRange &&
             Time.time >= shootTime)
         {
             Instantiate(bullet, transform.position, transform.rotation);
