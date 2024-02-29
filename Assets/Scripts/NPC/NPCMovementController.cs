23edff155f37344f653b4bf74d5655985c637ebe
adenprince <45126168+adenprince@users.noreply.github.com>
2024-02-28 22:06:54 -0600

    Make NPCMovementController.cs update NPC direction

--- Assets/Scripts/NPC/NPCMovementController.cs
+++ Assets/Scripts/NPC/NPCMovementController.cs
@@ -6,12 +6,14 @@ public class NPCMovementController : MonoBehaviour
     [SerializeField] private List<Vector2> targetPositions;
     [SerializeField] private float movementSpeed;
 
+    private CharacterDirectionController directionController;
     private new Rigidbody2D rigidbody2D;
     private Vector2 targetPosition;
     private int currentTargetPositionIndex;
 
     private void Awake()
     {
+        directionController = GetComponent<CharacterDirectionController>();
         rigidbody2D = GetComponent<Rigidbody2D>();
 
         targetPosition = targetPositions[currentTargetPositionIndex];
@@ -21,6 +23,19 @@ public class NPCMovementController : MonoBehaviour
     {
         Vector2 nextPosition = Vector2.MoveTowards(
             rigidbody2D.position, targetPosition, movementSpeed);
+
+        Vector2 velocity = nextPosition - rigidbody2D.position;
+
+        if (Mathf.Abs(velocity.y) > Mathf.Abs(velocity.x))
+        {
+            directionController.Direction = velocity.y > 0f ?
+                CharacterDirection.Up : CharacterDirection.Down;
+        }
+        else
+        {
+            directionController.Direction = velocity.x > 0f ?
+                CharacterDirection.Right : CharacterDirection.Left;
+        }
 
         rigidbody2D.MovePosition(nextPosition);
 
